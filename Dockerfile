e544d2f05f52c9864cfbd31842c05c0317e7a2b7
aliasccl <george@crosschainlabs.tech>
2022-11-03 11:26:44 +0200

    update Dockerfile and unit tests

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,14 @@
-FROM node:14-stretch AS development
+FROM node:alpine
 
-WORKDIR /usr/src/app
-COPY package*.json ./
+RUN apk add --update git
+
+WORKDIR '/app'
+
+COPY package.json .
 
 RUN npm install
-COPY . .
-
-FROM node:14-stretch AS production
-
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
-
-WORKDIR /usr/src/app
-COPY package*.json ./
-
-RUN npm update -g yarn npm
-RUN yarn install --production
 
 COPY . .
+COPY .env.sample .env
 
-CMD ["node", "index.js"]
+CMD ["npm", "run", "start"]
--- api.test.js
+++ api.test.js
@@ -62,7 +62,7 @@ describe("API", () => {
 
     const req = mockRequest();
     const res = mockResponse();
-    
+
     await statistics(req, res);
 
     expect(res.json).toHaveBeenCalledWith({
@@ -137,6 +137,522 @@ describe("API", () => {
         avatar_url: "avatar_url5",
         contributions: "500",
       },
+    ]);
+  });
+
+  test("commits", async () => {
+    pool.query.mockResolvedValue({
+      rows: [
+        {
+          commits: "1900",
+          commit_month: "2021-06-01T00:00:00.000Z",
+          display_month: "Jun 21",
+        },
+        {
+          commits: "2500",
+          commit_month: "2021-07-01T00:00:00.000Z",
+          display_month: "Jul 21",
+        },
+        {
+          commits: "2000",
+          commit_month: "2021-08-01T00:00:00.000Z",
+          display_month: "Aug 21",
+        },
+        {
+          commits: "1600",
+          commit_month: "2021-09-01T00:00:00.000Z",
+          display_month: "Sep 21",
+        },
+        {
+          commits: "1400",
+          commit_month: "2021-10-01T00:00:00.000Z",
+          display_month: "Oct 21",
+        },
+        {
+          commits: "1600",
+          commit_month: "2021-11-01T00:00:00.000Z",
+          display_month: "Nov 21",
+        },
+        {
+          commits: "1900",
+          commit_month: "2021-12-01T00:00:00.000Z",
+          display_month: "Dec 21",
+        },
+        {
+          commits: "2500",
+          commit_month: "2022-01-01T00:00:00.000Z",
+          display_month: "Jan 22",
+        },
+        {
+          commits: "2300",
+          commit_month: "2022-02-01T00:00:00.000Z",
+          display_month: "Feb 22",
+        },
+        {
+          commits: "3000",
+          commit_month: "2022-03-01T00:00:00.000Z",
+          display_month: "Mar 22",
+        },
+        {
+          commits: "4000",
+          commit_month: "2022-04-01T00:00:00.000Z",
+          display_month: "Apr 22",
+        },
+        {
+          commits: "3800",
+          commit_month: "2022-05-01T00:00:00.000Z",
+          display_month: "May 22",
+        },
+        {
+          commits: "3600",
+          commit_month: "2022-06-01T00:00:00.000Z",
+          display_month: "Jun 22",
+        },
+        {
+          commits: "2400",
+          commit_month: "2022-07-01T00:00:00.000Z",
+          display_month: "Jul 22",
+        },
+        {
+          commits: "4900",
+          commit_month: "2022-08-01T00:00:00.000Z",
+          display_month: "Aug 22",
+        },
+        {
+          commits: "5300",
+          commit_month: "2022-09-01T00:00:00.000Z",
+          display_month: "Sep 22",
+        },
+        {
+          commits: "4500",
+          commit_month: "2022-10-01T00:00:00.000Z",
+          display_month: "Oct 22",
+        },
+      ],
+    });
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    await commits(req, res);
+
+    expect(res.json).toHaveBeenCalledWith([
+      {
+        commits: "1900",
+        commit_month: "2021-06-01T00:00:00.000Z",
+        display_month: "Jun 21",
+      },
+      {
+        commits: "2500",
+        commit_month: "2021-07-01T00:00:00.000Z",
+        display_month: "Jul 21",
+      },
+      {
+        commits: "2000",
+        commit_month: "2021-08-01T00:00:00.000Z",
+        display_month: "Aug 21",
+      },
+      {
+        commits: "1600",
+        commit_month: "2021-09-01T00:00:00.000Z",
+        display_month: "Sep 21",
+      },
+      {
+        commits: "1400",
+        commit_month: "2021-10-01T00:00:00.000Z",
+        display_month: "Oct 21",
+      },
+      {
+        commits: "1600",
+        commit_month: "2021-11-01T00:00:00.000Z",
+        display_month: "Nov 21",
+      },
+      {
+        commits: "1900",
+        commit_month: "2021-12-01T00:00:00.000Z",
+        display_month: "Dec 21",
+      },
+      {
+        commits: "2500",
+        commit_month: "2022-01-01T00:00:00.000Z",
+        display_month: "Jan 22",
+      },
+      {
+        commits: "2300",
+        commit_month: "2022-02-01T00:00:00.000Z",
+        display_month: "Feb 22",
+      },
+      {
+        commits: "3000",
+        commit_month: "2022-03-01T00:00:00.000Z",
+        display_month: "Mar 22",
+      },
+      {
+        commits: "4000",
+        commit_month: "2022-04-01T00:00:00.000Z",
+        display_month: "Apr 22",
+      },
+      {
+        commits: "3800",
+        commit_month: "2022-05-01T00:00:00.000Z",
+        display_month: "May 22",
+      },
+      {
+        commits: "3600",
+        commit_month: "2022-06-01T00:00:00.000Z",
+        display_month: "Jun 22",
+      },
+      {
+        commits: "2400",
+        commit_month: "2022-07-01T00:00:00.000Z",
+        display_month: "Jul 22",
+      },
+      {
+        commits: "4900",
+        commit_month: "2022-08-01T00:00:00.000Z",
+        display_month: "Aug 22",
+      },
+      {
+        commits: "5300",
+        commit_month: "2022-09-01T00:00:00.000Z",
+        display_month: "Sep 22",
+      },
+      {
+        commits: "4500",
+        commit_month: "2022-10-01T00:00:00.000Z",
+        display_month: "Oct 22",
+      },
+    ]);
+  });
+
+  test("activeContributors", async () => {
+    pool.query.mockResolvedValue({
+      rows: [
+        {
+          active_contributors: "50",
+          month: "2021-06-01T00:00:00.000Z",
+          display_month: "Jun 21",
+        },
+        {
+          active_contributors: "130",
+          month: "2021-07-01T00:00:00.000Z",
+          display_month: "Jul 21",
+        },
+        {
+          active_contributors: "120",
+          month: "2021-08-01T00:00:00.000Z",
+          display_month: "Aug 21",
+        },
+        {
+          active_contributors: "120",
+          month: "2021-09-01T00:00:00.000Z",
+          display_month: "Sep 21",
+        },
+        {
+          active_contributors: "110",
+          month: "2021-10-01T00:00:00.000Z",
+          display_month: "Oct 21",
+        },
+        {
+          active_contributors: "110",
+          month: "2021-11-01T00:00:00.000Z",
+          display_month: "Nov 21",
+        },
+        {
+          active_contributors: "120",
+          month: "2021-12-01T00:00:00.000Z",
+          display_month: "Dec 21",
+        },
+        {
+          active_contributors: "130",
+          month: "2022-01-01T00:00:00.000Z",
+          display_month: "Jan 22",
+        },
+        {
+          active_contributors: "160",
+          month: "2022-02-01T00:00:00.000Z",
+          display_month: "Feb 22",
+        },
+        {
+          active_contributors: "165",
+          month: "2022-03-01T00:00:00.000Z",
+          display_month: "Mar 22",
+        },
+        {
+          active_contributors: "145",
+          month: "2022-04-01T00:00:00.000Z",
+          display_month: "Apr 22",
+        },
+        {
+          active_contributors: "140",
+          month: "2022-05-01T00:00:00.000Z",
+          display_month: "May 22",
+        },
+        {
+          active_contributors: "130",
+          month: "2022-06-01T00:00:00.000Z",
+          display_month: "Jun 22",
+        },
+        {
+          active_contributors: "130",
+          month: "2022-07-01T00:00:00.000Z",
+          display_month: "Jul 22",
+        },
+        {
+          active_contributors: "137",
+          month: "2022-08-01T00:00:00.000Z",
+          display_month: "Aug 22",
+        },
+        {
+          active_contributors: "135",
+          month: "2022-09-01T00:00:00.000Z",
+          display_month: "Sep 22",
+        },
+        {
+          active_contributors: "120",
+          month: "2022-10-01T00:00:00.000Z",
+          display_month: "Oct 22",
+        },
+      ],
+    });
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    await activeContributors(req, res);
+
+    expect(res.json).toHaveBeenCalledWith([
+      {
+        active_contributors: "50",
+        month: "2021-06-01T00:00:00.000Z",
+        display_month: "Jun 21",
+      },
+      {
+        active_contributors: "130",
+        month: "2021-07-01T00:00:00.000Z",
+        display_month: "Jul 21",
+      },
+      {
+        active_contributors: "120",
+        month: "2021-08-01T00:00:00.000Z",
+        display_month: "Aug 21",
+      },
+      {
+        active_contributors: "120",
+        month: "2021-09-01T00:00:00.000Z",
+        display_month: "Sep 21",
+      },
+      {
+        active_contributors: "110",
+        month: "2021-10-01T00:00:00.000Z",
+        display_month: "Oct 21",
+      },
+      {
+        active_contributors: "110",
+        month: "2021-11-01T00:00:00.000Z",
+        display_month: "Nov 21",
+      },
+      {
+        active_contributors: "120",
+        month: "2021-12-01T00:00:00.000Z",
+        display_month: "Dec 21",
+      },
+      {
+        active_contributors: "130",
+        month: "2022-01-01T00:00:00.000Z",
+        display_month: "Jan 22",
+      },
+      {
+        active_contributors: "160",
+        month: "2022-02-01T00:00:00.000Z",
+        display_month: "Feb 22",
+      },
+      {
+        active_contributors: "165",
+        month: "2022-03-01T00:00:00.000Z",
+        display_month: "Mar 22",
+      },
+      {
+        active_contributors: "145",
+        month: "2022-04-01T00:00:00.000Z",
+        display_month: "Apr 22",
+      },
+      {
+        active_contributors: "140",
+        month: "2022-05-01T00:00:00.000Z",
+        display_month: "May 22",
+      },
+      {
+        active_contributors: "130",
+        month: "2022-06-01T00:00:00.000Z",
+        display_month: "Jun 22",
+      },
+      {
+        active_contributors: "130",
+        month: "2022-07-01T00:00:00.000Z",
+        display_month: "Jul 22",
+      },
+      {
+        active_contributors: "137",
+        month: "2022-08-01T00:00:00.000Z",
+        display_month: "Aug 22",
+      },
+      {
+        active_contributors: "135",
+        month: "2022-09-01T00:00:00.000Z",
+        display_month: "Sep 22",
+      },
+      {
+        active_contributors: "120",
+        month: "2022-10-01T00:00:00.000Z",
+        display_month: "Oct 22",
+      },
+    ]);
+  });
+
+  test("recentCommits", async () => {
+    pool.query.mockResolvedValue({
+      rows: [
+        {
+          dev_name: "dev1",
+          repo: "repo1",
+          organisation: "org1",
+          commit_hash: "hash1",
+          commit_date: "date1",
+          avatar_url: "avatar_url1",
+          message: "message1",
+        },
+        {
+          dev_name: "dev2",
+          repo: "repo2",
+          organisation: "org2",
+          commit_hash: "hash2",
+          commit_date: "date2",
+          avatar_url: "avatar_url2",
+          message: "message2",
+        },
+        {
+          dev_name: "dev3",
+          repo: "repo3",
+          organisation: "org3",
+          commit_hash: "hash3",
+          commit_date: "date3",
+          avatar_url: "avatar_url3",
+          message: "message3",
+        },
+        {
+          dev_name: "dev4",
+          repo: "repo4",
+          organisation: "org4",
+          commit_hash: "hash4",
+          commit_date: "date4",
+          avatar_url: "avatar_url4",
+          message: "message4",
+        },
+        {
+          dev_name: "dev5",
+          repo: "repo5",
+          organisation: "org5",
+          commit_hash: "hash5",
+          commit_date: "date5",
+          avatar_url: "avatar_url5",
+          message: "message5",
+        },
+      ],
+    });
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    await recentCommits(req, res);
+
+    expect(res.json).toHaveBeenCalledWith([
+      {
+        dev_name: "dev1",
+        repo: "repo1",
+        organisation: "org1",
+        commit_hash: "hash1",
+        commit_date: "date1",
+        avatar_url: "avatar_url1",
+        message: "message1",
+      },
+      {
+        dev_name: "dev2",
+        repo: "repo2",
+        organisation: "org2",
+        commit_hash: "hash2",
+        commit_date: "date2",
+        avatar_url: "avatar_url2",
+        message: "message2",
+      },
+      {
+        dev_name: "dev3",
+        repo: "repo3",
+        organisation: "org3",
+        commit_hash: "hash3",
+        commit_date: "date3",
+        avatar_url: "avatar_url3",
+        message: "message3",
+      },
+      {
+        dev_name: "dev4",
+        repo: "repo4",
+        organisation: "org4",
+        commit_hash: "hash4",
+        commit_date: "date4",
+        avatar_url: "avatar_url4",
+        message: "message4",
+      },
+      {
+        dev_name: "dev5",
+        repo: "repo5",
+        organisation: "org5",
+        commit_hash: "hash5",
+        commit_date: "date5",
+        avatar_url: "avatar_url5",
+        message: "message5",
+      },
+    ]);
+  });
+
+  test("repositories", async () => {
+    pool.query.mockResolvedValue({
+      rows: [
+        { month: "2021-06-30T00:00:00.000Z", repositories: "150" },
+        { month: "2021-08-31T00:00:00.000Z", repositories: "160" },
+        { month: "2021-09-30T00:00:00.000Z", repositories: "170" },
+        { month: "2021-10-31T22:00:00.000Z", repositories: "180" },
+        { month: "2021-11-30T22:00:00.000Z", repositories: "190" },
+        { month: "2021-12-31T22:00:00.000Z", repositories: "200" },
+        { month: "2022-01-31T22:00:00.000Z", repositories: "220" },
+        { month: "2022-02-28T22:00:00.000Z", repositories: "240" },
+        { month: "2022-03-31T00:00:00.000Z", repositories: "260" },
+        { month: "2022-04-30T00:00:00.000Z", repositories: "280" },
+        { month: "2022-06-30T00:00:00.000Z", repositories: "300" },
+        { month: "2022-07-31T00:00:00.000Z", repositories: "350" },
+        { month: "2022-08-31T00:00:00.000Z", repositories: "400" },
+      ],
+    });
+
+    const req = mockRequest();
+    const res = mockResponse();
+
+    await repositories(req, res);
+
+    expect(res.json).toHaveBeenCalledWith([
+      { month: "2021-06-30T00:00:00.000Z", repositories: "150" },
+      { month: "2021-08-31T00:00:00.000Z", repositories: "160" },
+      { month: "2021-09-30T00:00:00.000Z", repositories: "170" },
+      { month: "2021-10-31T22:00:00.000Z", repositories: "180" },
+      { month: "2021-11-30T22:00:00.000Z", repositories: "190" },
+      { month: "2021-12-31T22:00:00.000Z", repositories: "200" },
+      { month: "2022-01-31T22:00:00.000Z", repositories: "220" },
+      { month: "2022-02-28T22:00:00.000Z", repositories: "240" },
+      { month: "2022-03-31T00:00:00.000Z", repositories: "260" },
+      { month: "2022-04-30T00:00:00.000Z", repositories: "280" },
+      { month: "2022-06-30T00:00:00.000Z", repositories: "300" },
+      { month: "2022-07-31T00:00:00.000Z", repositories: "350" },
+      { month: "2022-08-31T00:00:00.000Z", repositories: "400" },
     ]);
   });
 });
